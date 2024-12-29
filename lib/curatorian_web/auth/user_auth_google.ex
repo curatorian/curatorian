f19afa298b20fc43a4f47310e677d6ae21b2abce
chrisnaadhi <chrisnaadhip@gmail.com>
2024-12-29 17:51:59 +0700

    googe auth update

--- lib/curatorian_web/auth/user_auth_google.ex
+++ lib/curatorian_web/auth/user_auth_google.ex
@@ -51,7 +51,10 @@ defmodule CuratorianWeb.UserAuthGoogle do
         # Authorization succesful
         IO.inspect({user, token}, label: "user and token")
 
+        user_record = Curatorian.Accounts.get_user_by_email_or_register(user["email"])
+
         conn
+        |> CuratorianWeb.UserAuth.log_in_user(user_record)
         |> put_session(:google_user, user)
         |> put_session(:google_user_token, token)
         |> Phoenix.Controller.redirect(to: "/")
