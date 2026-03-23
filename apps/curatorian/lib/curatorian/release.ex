d9e6958f8dd481efd2a67152c1ec3f366c1107ba
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2026-03-24 01:01:54 +0700

    prepare for pi

--- apps/curatorian/lib/curatorian/release.ex
+++ apps/curatorian/lib/curatorian/release.ex
@@ -25,4 +25,19 @@ defmodule Curatorian.Release do
   defp load_app do
     Application.load(@app)
   end
+
+  def seed do
+    load_app()
+
+    # Run the seeds script if it exists
+    seeds_path = Application.app_dir(@app, "priv/repo/seeds.exs")
+    rbac_seeds_path = Application.app_dir(@app, "priv/repo/seeds_rbac.exs")
+
+    if File.exists?(seeds_path) and File.exists?(rbac_seeds_path) do
+      Code.eval_file(seeds_path)
+      Code.eval_file(rbac_seeds_path)
+    else
+      IO.puts("No seeds file found at #{seeds_path}")
+    end
+  end
 end
